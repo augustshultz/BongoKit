1e3da408780b0b1464045f351eaa24826f860966
augustshultz <augustshultz@gmail.com>
2019-01-27 12:01:26 -0600

    Adds an enum to create URLs for network calls

--- Sources/BongoKit/Networking/BongoURL.swift
+++ Sources/BongoKit/Networking/BongoURL.swift
@@ -4,12 +4,28 @@
 
 import Foundation
 
-public struct BongoURL {
-  private static let baseURLString = "https://api.bongo.org/"
+enum BongoURL {
+  case routeInfo(Int)
+  case routeList
+  case predictions(Int)
+  case stopList
+  case stopInfo(Int)
+  case buses(Int)
   
-  public let routeListURL = URL(string: baseURLString + "routes")
-  
-  func routeInfoURL(<#parameters#>) -> <#return type#> {
-    <#function body#>
+  var url: URL {
+    switch self {
+    case .routeInfo(let routeId):
+      return URL(string: "https://api.bongo.org/routes/\(routeId)")!
+    case .routeList:
+      return URL(string: "https://api.bongo.org/routes/")!
+    case .predictions(let stopId):
+      return URL(string: "https://api.bongo.org/predictions/\(stopId)")!
+    case .stopList:
+      return URL(string: "https://api.bongo.org/stops")!
+    case .stopInfo(let stopId):
+      return URL(string: "https://api.bongo.org/stops/\(stopId)")!
+    case .buses(let routeId):
+      return URL(string: "https://api.bongo.org/buses/\(routeId)")!
+    }
   }
 }
