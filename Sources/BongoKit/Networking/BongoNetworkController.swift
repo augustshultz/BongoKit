97ad0c8d62bdde0d9173ab5812677dc208723756
augustshultz <augustshultz@gmail.com>
2019-03-04 21:51:01 -0600

    Refactor for readability and add .idea to .gitignore

--- Sources/BongoKit/Networking/BongoNetworkController.swift
+++ Sources/BongoKit/Networking/BongoNetworkController.swift
@@ -13,25 +13,38 @@ public class BongoNetworkController {
   public init() {}
   
   public func stops() -> [Stop] {
-    return fetch(fromUrl: BongoURL.stopList.url)
+
+    let stops: [Stop] = fetch(fromUrl: BongoURL.stopList.url)
+
+    return stops
   }
   
   public func predictions(forStopNumber stopNumber: Int, inTimeInterval interval: Int = 60) -> [Prediction] {
-    return fetch(fromUrl: BongoURL.predictions(stopNumber).url).filter { $0.minutes < interval }
+
+    let predictions: [Prediction] = fetch(fromUrl: BongoURL.predictions(stopNumber).url)
+    let predictionsInTimeInterval = predictions.filter { $0.minutes < interval }
+
+    return predictionsInTimeInterval
   }
   
   private func decode<T: Decodable>(fromData data: Data) -> [T] {
+
     var decoded: [T] = []
+
     do {
       decoded = try jsonDecoder.decode([T].self, from: data)
     } catch let error as NSError {
       print("\(error)")
     }
+
     return decoded
   }
   
   private func fetch<T: Decodable>(fromUrl url: URL) -> [T] {
+
     let response = session.synchronousDataTask(with: url)
-    return decode(fromData: response.0!)
+    let decodedResponse: [T] = decode(fromData: response.0!)
+
+    return decodedResponse
   }
 }
