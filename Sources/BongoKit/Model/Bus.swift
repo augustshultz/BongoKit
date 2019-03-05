c31c3901fe088c5c01e040c49598374f29028b7b
augustshultz <augustshultz@gmail.com>
2019-03-04 22:01:01 -0600

    Refactor model names to match coding standards despite API preferences

--- Sources/BongoKit/Model/Bus.swift
+++ Sources/BongoKit/Model/Bus.swift
@@ -6,7 +6,11 @@ import Foundation
 
 public struct Bus {
   public let id: Int
-  public let lat: Int
-  public let lon: Int
+  public let latitude: Int
+  public let longitude: Int
   public let heading: Int
+
+  private  enum CodingKeys: String, CodingKey {
+    case id, latitude = "lat", longitude = "lon", heading
+  }
 }
--- Sources/BongoKit/Model/Route.swift
+++ Sources/BongoKit/Model/Route.swift
@@ -7,7 +7,11 @@ import Foundation
 public struct Route: Decodable {
   public let id: Int
   public let name: String
-  public let shortname: String
+  public let shortName: String
   public let color: String
   public let agency: String
+
+  private enum CodingKeys: String, CodingKey {
+    case id, name, shortName = "shortname", color, agency
+  }
 }
