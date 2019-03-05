292883e44ee0e2b3bdc89010936ac1ba3489828d
augustshultz <augustshultz@gmail.com>
2019-03-05 06:17:20 -0600

    Adds a basic test of the Prediction model

--- Tests/BongoKitTests/BongoKitTests.swift
+++ Tests/BongoKitTests/BongoKitTests.swift
@@ -2,14 +2,17 @@ import XCTest
 @testable import BongoKit
 
 final class BongoKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BongoKit().text, "Hello, World!")
-    }
+  
+  func testDecodePrediction() {
+    let json = "{\"routeid\":2326,\"name\":\"Lantern Park\",\"shortname\":\"lanternpark\",\"busid\":3274,\"busnumber\":\"114\",\"seconds\":707,\"minutes\":12,\"agency\":\"coralville\",\"agencyname\":\"Coralville Transit\"}"
+    
+    let jsonDecoder = JSONDecoder()
+    let prediction: Prediction? = try? jsonDecoder.decode(Prediction.self, from: json.data(using: .utf8)!)
+    
+    XCTAssertEqual(prediction!.name, "Lantern Park")
+  }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testExample", testDecodePrediction)
     ]
 }
