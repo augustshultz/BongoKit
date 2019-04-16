7d9c5bd2895dca9a39eb280a7901298e9e93170f
augustshultz <augustshultz@gmail.com>
2019-04-15 19:05:26 -0500

    Use Codextended

--- Sources/BongoKit/Networking/BongoNetworkController.swift
+++ Sources/BongoKit/Networking/BongoNetworkController.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import NetworkKit
+import Codextended
 
 public class BongoNetworkController {
 
@@ -17,31 +18,19 @@ public class BongoNetworkController {
   }
 
   public func synchronouslyFetchPredictions(forStopNumber stopNumber: Int, inTimeInterval interval: Int = 60) -> [Prediction] {
-
     let predictions: [Prediction] = synchronouslyFetch(fromUrl: BongoURL.predictions(stopNumber).url)
     let predictionsInTimeInterval = predictions.filter { $0.minutes < interval }
-
     return predictionsInTimeInterval
   }
 
-  private func decode<T: Decodable>(fromData data: Data) -> [T] {
-
-    var decoded: [T] = []
-
+  private func synchronouslyFetch<T: Decodable>(fromUrl url: URL) -> [T] {
+    let response = session.synchronousDataTask(with: url)
+    var decodedResponse: [T] = []
     do {
-      decoded = try jsonDecoder.decode([T].self, from: data)
+      decodedResponse = try response.0!.decoded() as [T]
     } catch let error as NSError {
       print("\(error)")
     }
-
-    return decoded
-  }
-
-  private func synchronouslyFetch<T: Decodable>(fromUrl url: URL) -> [T] {
-
-    let response = session.synchronousDataTask(with: url)
-    let decodedResponse: [T] = decode(fromData: response.0!)
-
     return decodedResponse
   }
 }
