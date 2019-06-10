b4380363b95ef6559da5f356beb0f721b35c6e90
augustshultz <augustshultz@gmail.com>
2019-06-10 09:34:53 -0500

    Remove all synchronous fetching logic

--- Package.swift
+++ Package.swift
@@ -12,14 +12,13 @@ let package = Package(
       targets: ["BongoKit"])
   ],
   dependencies: [
-    .package(url: "https://github.com/augustshultz/NetworkKit.git", from: "0.1.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "BongoKit",
-      dependencies: ["NetworkKit"]),
+      dependencies: []),
     .testTarget(
       name: "BongoKitTests",
       dependencies: ["BongoKit"])
--- Sources/BongoKit/Networking/BongoNetworkController.swift
+++ Sources/BongoKit/Networking/BongoNetworkController.swift
@@ -3,10 +3,13 @@
 //
 
 import Foundation
-import NetworkKit
 
 public enum StopsError: Error {
   case failedToFetchStops
+}
+
+public enum PredictionsError: Error {
+  case failedToFetchPredictions
 }
 
 public class BongoNetworkController {
@@ -14,10 +17,6 @@ public class BongoNetworkController {
   private let session = URLSession.shared
 
   public init() {}
-
-  public func synchronouslyFetchStops() -> [Stop] {
-    return synchronouslyFetch(fromUrl: BongoURL.stopList.url)
-  }
   
   public func fetchStops(_ result: @escaping (Result<[Stop], Error>) -> Void) {
     let dataTask = session.dataTask(with: BongoURL.stopList.url) { (data, response, error) in
@@ -39,25 +38,26 @@ public class BongoNetworkController {
     }
     dataTask.resume()
   }
-
-  public func synchronouslyFetchPredictions(forStopNumber stopNumber: Int, inTimeInterval interval: Int = 60) -> [Prediction] {
-    let predictions: [Prediction] = synchronouslyFetch(fromUrl: BongoURL.predictions(stopNumber).url)
-    let predictionsInTimeInterval = predictions.filter { $0.minutes < interval }
-    return predictionsInTimeInterval
-  }
-
-  private func synchronouslyFetch<T: Decodable>(fromUrl url: URL) -> [T] {
-    let response = session.synchronousDataTask(with: url)
-    guard let data = response.0 else {
-      return []
+  
+  public func fetchPredictions(forStopNumber stopNumber: Int, _ result: @escaping (Result<[Prediction],Error>) -> Void) {
+    let dataTask = session.dataTask(with: BongoURL.predictions(stopNumber).url) { (data, response, error) in
+      if let error = error {
+        result(.failure(error))
+        return
+      }
+      guard let data = data else {
+        result(.failure(PredictionsError.failedToFetchPredictions))
+        return
+      }
+      do {
+        let decoder = JSONDecoder()
+        let predictions = try decoder.decode([Prediction].self, from: data)
+        result(.success(predictions))
+      } catch let error {
+        result(.failure(error))
+        return
+      }
     }
-    var decodedResponse: [T] = []
-    do {
-      let decoder = JSONDecoder()
-      decodedResponse = try decoder.decode([T].self, from: data)
-    } catch let error as NSError {
-      print("\(error)")
-    }
-    return decodedResponse
+    dataTask.resume()
   }
 }
