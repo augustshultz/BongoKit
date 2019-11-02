c5ba0ea5473468707ab89b3e29b13aed17282ffc
August Shultz <augustshultz@gmail.com>
2019-11-01 21:02:18 -0500

    Update Route property to meet linter standards for field name length

--- Sources/BongoKit/Model/Route.swift
+++ Sources/BongoKit/Model/Route.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 public struct Route: Decodable {
-  public let id: Int
+  public let routeId: Int
   public let name: String
   public let shortName: String
   public let color: String
@@ -13,6 +13,6 @@ public struct Route: Decodable {
   public let agencyName: String
 
   private enum CodingKeys: String, CodingKey {
-    case id, name, shortName = "shortname", color, agency, agencyName = "agencyname"
+    case routeId = "id", name, shortName = "shortname", color, agency, agencyName = "agencyname"
   }
 }
