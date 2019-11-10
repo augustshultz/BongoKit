39d0a6ccf0d611f7d9bfabf3a66ff6743d5c6868
August Shultz <augustshultz@gmail.com>
2019-11-10 09:47:28 -0600

    Pull JSON test data from file

--- Tests/BongoKitTests/Networking Tests/RoutesTest.swift
+++ Tests/BongoKitTests/Networking Tests/RoutesTest.swift
@@ -9,9 +9,13 @@ class RoutesTest: XCTestCase {
 
     func testFetchRoutes() {
 
-        guard let jsonData = routesJson.data(using: .utf8) else {
-            fatalError("Could not convert string contents into data")
+        let bundle = Bundle(for: type(of: self))
+
+        guard let url = bundle.url(forResource: "routes", withExtension: "json"), let jsonData = try? Data(contentsOf: url, options: .uncached) else {
+            XCTFail("Could not get file data")
+            fatalError()
         }
+
         let mockSession = MockURLSession(data: jsonData, urlResponse: nil, error: nil)
         let networkController = BongoNetworkController(session: mockSession)
         let fetchRoutesExpectation = expectation(description: "wait for predictions to be fetched")
@@ -29,34 +33,4 @@ class RoutesTest: XCTestCase {
             XCTAssertNil(error)
         }
     }
-
 }
-
-private let routesJson = """
-[
-  {
-    "id": 2336,
-    "name": "7th Avenue",
-    "shortname": "7thave",
-    "color": "#FFC53D",
-    "agency": "iowacity",
-    "agencyname": "Iowa City Transit"
-  },
-  {
-    "id": 2337,
-    "name": "Broadway",
-    "shortname": "brdwy",
-    "color": "#127AE3",
-    "agency": "iowacity",
-    "agencyname": "Iowa City Transit"
-  },
-  {
-    "id": 2338,
-    "name": "Broadway Night and Saturday",
-    "shortname": "brdwynw",
-    "color": "#783730",
-    "agency": "iowacity",
-    "agencyname": "Iowa City Transit"
-  }
-]
-"""
