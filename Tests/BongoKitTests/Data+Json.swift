253473237ec86a01c6dbf113163f81b4439f1289
August Shultz <augustshultz@gmail.com>
2019-11-12 20:40:34 -0600

    Extend Data to make creating json data objects easier

---
+++ Tests/BongoKitTests/Data+Json.swift
@@ -0,0 +1,18 @@
+//
+//  Created by August on 11/12/19.
+//
+
+import Foundation
+
+extension Data {
+    init(dataFromJsonFileNamed fileName: String, forBundle bundle: Bundle) throws {
+        guard let url = bundle.url(forResource: fileName, withExtension: "json") else {
+            throw JsonDataError.couldNotFindFile
+        }
+        self = try Data(contentsOf: url)
+    }
+}
+
+enum JsonDataError: Error {
+    case couldNotFindFile
+}
---
+++ Tests/BongoKitTests/Model Tests/Route/RouteTest.swift
@@ -0,0 +1,29 @@
+//
+//  Created by August Shultz on 3/6/19.
+//
+
+import XCTest
+@testable import BongoKit
+
+class RouteTest: XCTestCase {
+
+    let jsonDecoder = JSONDecoder()
+
+    func testShortNameDecode() throws {
+        let json = try Data(dataFromJsonFileNamed: "single_route", forBundle: Bundle(for: type(of: self)))
+        let route = try jsonDecoder.decode(Route.self, from: json)
+        XCTAssertEqual(route.shortName, "courthill")
+    }
+
+    func testAgencyNameDecode() throws {
+        let json = try Data(dataFromJsonFileNamed: "single_route", forBundle: Bundle(for: type(of: self)))
+        let route = try jsonDecoder.decode(Route.self, from: json)
+        XCTAssertEqual(route.agencyName, "Iowa City Transit")
+    }
+
+    func testDecodingMultipleRoutesFromJSON() throws {
+        let json = try Data(dataFromJsonFileNamed: "multiple_routes", forBundle: Bundle(for: type(of: self)))
+        let actualRoutes = try jsonDecoder.decode([Route].self, from: json)
+        XCTAssert(actualRoutes.count == 2)
+    }
+}
