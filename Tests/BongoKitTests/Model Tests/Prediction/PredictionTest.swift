cefe24cb60d57c6962eef71d1cf11d366555c8a9
August Shultz <augustshultz@gmail.com>
2019-11-13 07:07:13 -0600

    Pull Prediction json data for testing into a json file

---
+++ Tests/BongoKitTests/Model Tests/Prediction/PredictionTest.swift
@@ -0,0 +1,29 @@
+//
+//  Created by August Shultz on 3/5/19.
+//
+
+import XCTest
+@testable import BongoKit
+
+class PredictionTest: XCTestCase {
+
+    let jsonDecoder = JSONDecoder()
+
+    func testPredictionNameDecode() throws {
+        let json = try Data(fromJsonFileNamed: "single_prediction", forBundle: Bundle(for: type(of: self)))
+        let prediction = try jsonDecoder.decode(Prediction.self, from: json)
+        XCTAssertEqual(prediction.name, "Lantern Park")
+    }
+
+    func testPredictionShortNameDecode() throws {
+        let json = try Data(fromJsonFileNamed: "single_prediction", forBundle: Bundle(for: type(of: self)))
+        let prediction = try jsonDecoder.decode(Prediction.self, from: json)
+        XCTAssertEqual(prediction.shortName, "lanternpark")
+    }
+
+    func testPredictionAdencyNameDecode() throws {
+        let json = try Data(fromJsonFileNamed: "single_prediction", forBundle: Bundle(for: type(of: self)))
+        let prediction = try jsonDecoder.decode(Prediction.self, from: json)
+        XCTAssertEqual(prediction.agencyName, "Coralville Transit")
+    }
+}
