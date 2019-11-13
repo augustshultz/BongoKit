647314e5f51d8a1490a321a0a804d40cbe9ad064
August Shultz <augustshultz@gmail.com>
2019-11-13 07:02:54 -0600

    Pull Bus json data for testing into a json file

---
+++ Tests/BongoKitTests/Model Tests/Bus/BusTests.swift
@@ -0,0 +1,17 @@
+//
+//  Created by August Shultz on 11/1/19.
+//
+
+import XCTest
+
+@testable import BongoKit
+
+class BusTests: XCTestCase {
+
+    func testDecodeBusJson() throws {
+        let decoder = JSONDecoder()
+        let json = try Data(fromJsonFileNamed: "single_bus", forBundle: Bundle(for: type(of: self)))
+        let actual = try decoder.decode(Bus.self, from: json)
+        XCTAssertEqual(actual.id, 2915)
+    }
+}
